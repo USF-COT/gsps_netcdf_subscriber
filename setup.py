b4c6cbcbf86f0bdcc70c94e39346cd2dc8c6db2c
Michael Lindemuth <lindemuth@gmail.com>
2014-03-03 15:48:29 -0500

    Initial import

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Glider Singleton Publishing Service NetCDF Subscriber',
+    version='1.0',
+    author='Michael Lindemuth',
+    author_email='mlindemu@usf.edu',
+    packages=['gsps_netcdf_subscriber']
+)
